42b3a13903875472b3e28e05bc2b07cdc4f3914b
Yuichi <c0a230660d@edu.teu.ac.jp>
2024-04-16 16:50:40 +0900

    練習1

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #練習1
+    bg_img2 = pg.transform.flip(bg_img,True,False)#練習7-1
     kk_img = pg.image.load("fig/3.png") #練習2
     kk_img = pg.transform.flip(kk_img,True,False) #練習2
     tmr = 0
@@ -20,6 +21,9 @@ def main():
         x = tmr%800
         #print(tmr, x)
         screen.blit(bg_img, [-x, 0]) #練習6
+        screen.blit(bg_img2,[-x+1600, 0])#練習7-1
+        screen.blit(bg_img2,[-x+3200, 0])#練習7-2
+        screen.blit(bg_img2,[-x+4800, 0])#練習7-2
         screen.blit(kk_img,[300, 200]) #練習4
         pg.display.update()
         tmr += 1        
