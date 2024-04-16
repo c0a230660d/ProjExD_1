e9d4aff7313a07e0f74a96845c0b21ee849572c6
Yuichi <c0a230660d@edu.teu.ac.jp>
2024-04-16 16:32:00 +0900

    練習5

---
+++ pygame_image.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg") #練習1
+    kk_img = pg.image.load("fig/3.png") #練習2
+    kk_img = pg.transform.flip(kk_img,True,False) #練習2
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        x = tmr%800
+        #print(tmr, x)
+        screen.blit(bg_img, [-x, 0]) #練習6
+        screen.blit(kk_img,[300, 200]) #練習4
+        pg.display.update()
+        tmr += 1        
+        clock.tick(200) #練習5
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
